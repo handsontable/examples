755c7664b2ca156475f302aa464c1dcde1dbf3c0
Wojciech Czerniak <wojciech.czerniak@gmail.com>
2020-01-17 12:52:00 +0100

    Add examples for testing Handsontable pull requests.

---
+++ react/pull-request/src/index.js
@@ -0,0 +1,51 @@
+import '../node_modules/handsontable/dist/handsontable.full.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Handsontable from 'handsontable';
+import { HotTable } from '@handsontable/react';
+
+function getDebugInfo() {
+  let debug = 'Handsontable:';
+  debug += ` v${Handsontable.version}`;
+  debug += ` (${Handsontable.buildDate})`;
+  debug += ' Wrapper:';
+  debug += ` v${HotTable.version}`;
+  debug += ' React:';
+  debug += ` v${React.version}`;
+  return debug;
+}
+
+const data = Handsontable.helper.createSpreadsheetData(50, 25);
+const settings = {
+  colHeaders: true,
+  rowHeaders: true,
+  width: 600,
+  height: 500,
+  contextMenu: true,
+  manualColumnFreeze: true,
+  fixedRowsTop: 3,
+  fixedColumnsLeft: 3,
+  fixedRowsBottom: 3,
+  manualColumnMove: true,
+  manualColumnResize: true,
+  manualRowResize: true,
+  manualRomMove: true,
+  dropdownMenu: true,
+  filters: true,
+  columnSorting: true,
+  comments: true,
+  licenseKey: 'non-commercial-and-evaluation',
+};
+
+function App() {
+  return <HotTable data={data} settings={settings} />;
+}
+
+const rootElement = document.getElementById('app');
+ReactDOM.render(
+  <div>
+    <App />
+    {getDebugInfo()}
+  </div>,
+  rootElement,
+);
---
+++ vue/pull-request/README.md
@@ -0,0 +1,3 @@
+For testing Pull Request in Handsontable main repository
+
+https://codesandbox.io/s/github/handsontable/examples/tree/master/vue/pull-request
