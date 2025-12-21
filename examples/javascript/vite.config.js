88f8293ca8252200900da871290044fd8a96a82e
Georgi Tsochev <pebsconsulting@users.noreply.github.com>
2025-12-21 02:22:42 +0000

    chore: add manualChunks to split vendor bundles (vendor-handsontable/vendor-polyfills/vendor-core)

--- examples/javascript/vite.config.js
+++ examples/javascript/vite.config.js
@@ -1,5 +1,27 @@
 import { defineConfig } from "vite";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   base: "./",
+  plugins: [
+    visualizer({ filename: 'dist/stats.html', open: false })
+  ],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('/node_modules/handsontable')) return 'vendor-handsontable';
+            if (id.includes('/node_modules/hyperformula')) return 'vendor-hyperformula';
+            if (id.includes('/node_modules/core-js')) return 'vendor-polyfills';
+            if (id.match(/node_modules\/(react|react-dom|lodash|date-fns)\b/)) {
+              return 'vendor-core';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 });
