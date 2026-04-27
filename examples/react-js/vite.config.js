22a1abce17740d8a148f7f87b2e671ead67c6155
Mateusz Wojczal <mateusz@qunabu.com>
2026-04-27 15:09:16 +0200

    tp id

---
+++ examples/react-js/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  server: {
+    port: 8080,
+    allowedHosts: true,
+  },
+});
